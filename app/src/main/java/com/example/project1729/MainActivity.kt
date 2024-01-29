396dfb9816f6d698676d1f4ddb8406d45a56d120
leonid <lvravdin@edu.hse.ru>
2024-01-29 21:39:59 +0300

    #3 Доработка главного экрана

--- app/src/main/java/com/example/project1729/MainActivity.kt
+++ app/src/main/java/com/example/project1729/MainActivity.kt
@@ -1,131 +1,173 @@
 package com.example.project1729
 
+import android.annotation.SuppressLint
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.Button
 import android.widget.ScrollView
+import com.example.project1729.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
 
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        val testingEyeButton1 = findViewById<Button>(R.id.testingEyeButton1)
-        val testingEyeButton2 = findViewById<Button>(R.id.testingEyeButton2)
-
-        val measurementMethodButton1 = findViewById<Button>(R.id.measurementMethodButton1)
-        val measurementMethodButton2 = findViewById<Button>(R.id.measurementMethodButton2)
-        val measurementMethodButton3 = findViewById<Button>(R.id.measurementMethodButton3)
-
-        val radiationIntensityButton1 = findViewById<Button>(R.id.radiationIntensityButton1)
-        val radiationIntensityButton2 = findViewById<Button>(R.id.radiationIntensityButton2)
-        val radiationIntensityButton3 = findViewById<Button>(R.id.radiationIntensityButton3)
-
-
-        testingEyeButton1.setOnClickListener {
+        binding.testingEyeButton1.setOnClickListener {
             testingEyeButtonPressed(1)
         }
 
-        testingEyeButton2.setOnClickListener {
+        binding.testingEyeButton2.setOnClickListener {
             testingEyeButtonPressed(2)
         }
 
 
-        measurementMethodButton1.setOnClickListener {
+        binding.measurementMethodButton1.setOnClickListener {
             measurementMethodButtonPressed(1)
         }
 
-        measurementMethodButton2.setOnClickListener {
+        binding.measurementMethodButton2.setOnClickListener {
             measurementMethodButtonPressed(2)
         }
 
-        measurementMethodButton3.setOnClickListener {
+        binding.measurementMethodButton3.setOnClickListener {
             measurementMethodButtonPressed(3)
         }
 
 
-        radiationIntensityButton1.setOnClickListener {
+        binding.radiationIntensityButton1.setOnClickListener {
             radiationIntensityButtonPressed(1)
         }
 
-        radiationIntensityButton2.setOnClickListener {
+        binding.radiationIntensityButton2.setOnClickListener {
             radiationIntensityButtonPressed(2)
         }
 
-        radiationIntensityButton3.setOnClickListener {
+        binding.radiationIntensityButton3.setOnClickListener {
             radiationIntensityButtonPressed(3)
+        }
+
+        binding.redButton.setOnClickListener {
+            diodeColorChanged(1)
+        }
+
+        binding.greenButton.setOnClickListener {
+            diodeColorChanged(2)
+        }
+
+        binding.blueButton.setOnClickListener {
+            diodeColorChanged(3)
+        }
+
+        binding.whiteButton.setOnClickListener {
+            diodeColorChanged(4)
         }
     }
 
 
     fun testingEyeButtonPressed(btnNum: Int){
-        val btn1 = findViewById<Button>(R.id.testingEyeButton1)
-        val btn2 = findViewById<Button>(R.id.testingEyeButton2)
-        if (btnNum == 1){
-            btn1.setBackgroundColor(getResources().getColor(R.color.white))
-            btn2.setBackgroundColor(getResources().getColor(R.color.grey1))
+        val btn1 = binding.testingEyeButton1
+        val btn2 = binding.testingEyeButton2
+        when (btnNum) {
+            1 -> {
+                btn1.setBackgroundColor(getResources().getColor(R.color.white))
+                btn2.setBackgroundColor(getResources().getColor(R.color.grey1))
 
-        }
-        if (btnNum == 2){
-            btn1.setBackgroundColor(getResources().getColor(R.color.grey1))
-            btn2.setBackgroundColor(getResources().getColor(R.color.white))
+            }
+            2 -> {
+                btn1.setBackgroundColor(getResources().getColor(R.color.grey1))
+                btn2.setBackgroundColor(getResources().getColor(R.color.white))
 
+            }
         }
     }
 
     fun measurementMethodButtonPressed(btnNum: Int){
-        val btn1 = findViewById<Button>(R.id.measurementMethodButton1)
-        val btn2 = findViewById<Button>(R.id.measurementMethodButton2)
-        val btn3 = findViewById<Button>(R.id.measurementMethodButton3)
+        val btn1 = binding.measurementMethodButton1
+        val btn2 = binding.measurementMethodButton2
+        val btn3 = binding.measurementMethodButton3
 
-        if (btnNum == 1){
-            btn1.setBackgroundColor(getResources().getColor(R.color.white))
-            btn2.setBackgroundColor(getResources().getColor(R.color.grey1))
-            btn3.setBackgroundColor(getResources().getColor(R.color.grey1))
+        when (btnNum) {
+            1 -> {
+                btn1.setBackgroundColor(getResources().getColor(R.color.white))
+                btn2.setBackgroundColor(getResources().getColor(R.color.grey1))
+                btn3.setBackgroundColor(getResources().getColor(R.color.grey1))
 
-        }
-        if (btnNum == 2){
-            btn1.setBackgroundColor(getResources().getColor(R.color.grey1))
-            btn2.setBackgroundColor(getResources().getColor(R.color.white))
-            btn3.setBackgroundColor(getResources().getColor(R.color.grey1))
+            }
+            2 -> {
+                btn1.setBackgroundColor(getResources().getColor(R.color.grey1))
+                btn2.setBackgroundColor(getResources().getColor(R.color.white))
+                btn3.setBackgroundColor(getResources().getColor(R.color.grey1))
 
-        }
+            }
 
-        if (btnNum == 3){
-            btn1.setBackgroundColor(getResources().getColor(R.color.grey1))
-            btn2.setBackgroundColor(getResources().getColor(R.color.grey1))
-            btn3.setBackgroundColor(getResources().getColor(R.color.white))
+            3 -> {
+                btn1.setBackgroundColor(getResources().getColor(R.color.grey1))
+                btn2.setBackgroundColor(getResources().getColor(R.color.grey1))
+                btn3.setBackgroundColor(getResources().getColor(R.color.white))
 
+            }
         }
     }
 
     fun radiationIntensityButtonPressed(btnNum: Int){
-        val btn1 = findViewById<Button>(R.id.radiationIntensityButton1)
-        val btn2 = findViewById<Button>(R.id.radiationIntensityButton2)
-        val btn3 = findViewById<Button>(R.id.radiationIntensityButton3)
+        val btn1 = binding.radiationIntensityButton1
+        val btn2 = binding.radiationIntensityButton2
+        val btn3 = binding.radiationIntensityButton3
 
-        if (btnNum == 1){
-            btn1.setBackgroundColor(getResources().getColor(R.color.white))
-            btn2.setBackgroundColor(getResources().getColor(R.color.grey1))
-            btn3.setBackgroundColor(getResources().getColor(R.color.grey1))
+        when (btnNum) {
+            1 -> {
+                btn1.setBackgroundColor(getResources().getColor(R.color.white))
+                btn2.setBackgroundColor(getResources().getColor(R.color.grey1))
+                btn3.setBackgroundColor(getResources().getColor(R.color.grey1))
 
+            }
+            2 -> {
+                btn1.setBackgroundColor(getResources().getColor(R.color.grey1))
+                btn2.setBackgroundColor(getResources().getColor(R.color.white))
+                btn3.setBackgroundColor(getResources().getColor(R.color.grey1))
+
+            }
+
+            3 -> {
+                btn1.setBackgroundColor(getResources().getColor(R.color.grey1))
+                btn2.setBackgroundColor(getResources().getColor(R.color.grey1))
+                btn3.setBackgroundColor(getResources().getColor(R.color.white))
+
+            }
         }
-        if (btnNum == 2){
-            btn1.setBackgroundColor(getResources().getColor(R.color.grey1))
-            btn2.setBackgroundColor(getResources().getColor(R.color.white))
-            btn3.setBackgroundColor(getResources().getColor(R.color.grey1))
+    }
 
-        }
 
-        if (btnNum == 3){
-            btn1.setBackgroundColor(getResources().getColor(R.color.grey1))
-            btn2.setBackgroundColor(getResources().getColor(R.color.grey1))
-            btn3.setBackgroundColor(getResources().getColor(R.color.white))
+    @SuppressLint("UseCompatLoadingForDrawables")
+    fun diodeColorChanged(btnNum: Int){
+        binding.redButton.background = getDrawable(R.drawable.red_btn_inactive)
+        binding.greenButton.background = getDrawable(R.drawable.green_btn_inactive)
+        binding.blueButton.background = getDrawable(R.drawable.blue_btn_inactive)
+        binding.whiteButton.background = getDrawable(R.drawable.white_btn_inactive)
+        when (btnNum){
+            1 -> {
+                binding.redButton.background = getDrawable(R.drawable.red_btn_active)
+            }
 
+            2 -> {
+                binding.greenButton.background = getDrawable(R.drawable.green_btn_active)
+            }
+
+            3 -> {
+                binding.blueButton.background = getDrawable(R.drawable.blue_btn_active)
+            }
+
+            4 -> {
+                binding.whiteButton.background = getDrawable(R.drawable.white_btn_active)
+            }
         }
     }
 
